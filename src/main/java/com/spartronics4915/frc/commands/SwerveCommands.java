e6e17f012ef6638cdafb9152acfb257dab01d18f
ss0g <55930267+ss0g@users.noreply.github.com>
2022-11-18 16:39:58 -0800

    initial commit

---
+++ src/main/java/com/spartronics4915/frc/commands/SwerveCommands.java
@@ -0,0 +1,102 @@
+package com.spartronics4915.frc.commands;
+
+import com.spartronics4915.frc.subsystems.Swerve;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+import static com.spartronics4915.frc.Constants.OI.*;
+
+public class SwerveCommands {
+    private final XboxController mController;
+
+    private final Swerve mSwerve;
+
+    public SwerveCommands(XboxController controller, Swerve swerve) {
+        mController = controller;
+        mSwerve = swerve;
+    }
+
+    public class TeleopCommand extends CommandBase {
+        public TeleopCommand() {
+            addRequirements(mSwerve);
+        }
+
+        @Override
+        public void initialize() {}
+
+        @Override
+        public void execute() {
+            double x1 = mController.getLeftX();
+            double y1 = mController.getLeftY();
+            double x2 = mController.getRightX();
+
+            x1 = applyTransformations(x1);
+            y1 = applyTransformations(y1);
+            x2 = applyTransformations(x2);
+
+            Translation2d translation = new Translation2d(-y1, -x1);
+            
+            mSwerve.drive(translation, x2, !mController.getRawButton(kRobotOrientedButton), true);
+        }
+
+        @Override
+        public void end(boolean interrupted) {}
+
+        @Override
+        public boolean isFinished() {
+            return false;
+        }
+    }
+
+    public class TestingCommand extends CommandBase {
+        private double mDesiredAngle;
+        private SwerveModuleState[] mDesiredStates;
+        
+        public TestingCommand() {
+            addRequirements(mSwerve);
+        }
+
+        @Override
+        public void initialize() {
+            mSwerve.resetTestingAngle();
+            mDesiredAngle = 0;
+            mDesiredStates = new SwerveModuleState[4];
+            for (SwerveModuleState s : mDesiredStates) {
+                s = new SwerveModuleState(0, new Rotation2d(mDesiredAngle));
+            }
+            mSwerve.setModuleStates(mDesiredStates);
+        }
+
+        @Override
+        public void execute() {
+            if (mController.getRawButtonPressed(kTestingButton)) {
+                mDesiredAngle = mSwerve.getTestingAngleSupplier().getAsDouble();
+                for (SwerveModuleState s : mDesiredStates) {
+                    s = new SwerveModuleState(0, new Rotation2d(mDesiredAngle));
+                }
+                mSwerve.setModuleStates(mDesiredStates);
+            } // does this work?
+        }
+
+        @Override
+        public void end(boolean interrupted) {}
+
+        @Override
+        public boolean isFinished() {
+            return false;
+        }
+    }
+
+    private double applyTransformations(double c) {
+        return applyResponseCurve(MathUtil.applyDeadband(c, kDeadband));
+    }
+
+    private double applyResponseCurve(double c) {
+        return Math.signum(c) * Math.pow(Math.abs(c), kResponseCurveExponent);
+    }
+}
---
+++ src/main/java/com/spartronics4915/frc/subsystems/Swerve.java
@@ -0,0 +1,146 @@
+package com.spartronics4915.frc.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static com.spartronics4915.frc.Constants.Swerve.*;
+
+import java.util.function.DoubleSupplier;
+
+public class Swerve extends SubsystemBase {
+    private SwerveDriveOdometry mOdometry;
+    private SwerveModule[] mModules;
+    private AHRS mNavX;
+
+    /**
+     * for testing
+     */
+    private DoubleSupplier mTestingAngleSupplier = new DoubleSupplier() {
+        private int mCount = 0;
+        private double mAngle = 0;
+
+        @Override
+        public double getAsDouble() {
+            mCount++;
+            mAngle = mCount * Math.PI / 4;
+            mAngle = mAngle >= 2.0 * Math.PI ? (2.0 * Math.PI) % mAngle : mAngle;
+            return mAngle;
+        }
+
+        public int getCount() {
+            return mCount;
+        }
+
+        public void reset() {
+            mCount = 0;
+            mAngle = 0;
+        }
+    };
+
+    public Swerve() {
+        mNavX = new AHRS();
+        mNavX.reset();
+
+        mOdometry = new SwerveDriveOdometry(kKinematics, getYaw());
+
+        mModules = new SwerveModule[] {
+            new SwerveModule(0, Module0.kConstants),
+            new SwerveModule(1, Module1.kConstants),
+            new SwerveModule(2, Module2.kConstants),
+            new SwerveModule(3, Module3.kConstants)
+        };
+    }
+
+    public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
+        ChassisSpeeds chassisSpeeds;
+        if (fieldRelative) {
+            chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
+                translation.getX(),
+                translation.getY(),
+                rotation * kMaxAngularSpeed,
+                getYaw()
+            );
+        } else {
+            chassisSpeeds = new ChassisSpeeds(
+                translation.getX(),
+                translation.getY(),
+                rotation * kMaxAngularSpeed
+            );
+        }
+
+        SwerveModuleState[] moduleStates = kKinematics.toSwerveModuleStates(chassisSpeeds);
+        
+        kKinematics.desaturateWheelSpeeds(moduleStates, kMaxSpeed);
+
+        for (SwerveModule mod : mModules) {
+            mod.setDesiredState(moduleStates[mod.getModuleNumber()], isOpenLoop);
+        }
+    }
+
+    public void setModuleStates(SwerveModuleState[] desiredStates) {
+        kKinematics.desaturateWheelSpeeds(desiredStates, kMaxSpeed);
+
+        for (SwerveModule mod : mModules) {
+            mod.setDesiredState(desiredStates[mod.getModuleNumber()], false);
+        }
+    }
+
+    public Pose2d getPose() {
+        return mOdometry.getPoseMeters();
+    }
+
+    public Rotation2d getYaw() {
+        return Rotation2d.fromDegrees(mNavX.getYaw());
+    }
+
+    public void resetOdometry(Pose2d pose) {
+        mOdometry.resetPosition(pose, getYaw());
+    }
+
+    public void resetModuleZeroes() {
+        for (SwerveModule mod : mModules) {
+            mod.resetToAbsolute();
+        }
+    }
+
+    public SwerveModuleState[] getStates() {
+        SwerveModuleState[] states = new SwerveModuleState[4];
+        for (SwerveModule mod : mModules) {
+            states[mod.getModuleNumber()] = mod.getState();
+        }
+        return states;
+    }
+
+    public void zeroNavX() {
+        mNavX.reset();
+    }
+
+    public DoubleSupplier getTestingAngleSupplier() {
+        return mTestingAngleSupplier;
+    }
+
+    public void resetTestingAngle() {
+        try {
+            mTestingAngleSupplier.getClass().getMethod("reset").invoke(mTestingAngleSupplier); // cant call this method normally because it is an anonymous class
+        } catch (Exception e) {
+            System.err.println("Failed to reset swerve testing angle");
+            e.printStackTrace(System.err);
+        }
+    }
+
+    @Override
+    public void periodic() {
+        mOdometry.update(getYaw(), getStates());
+        for (SwerveModule mod : mModules) {
+            mod.putSmartDashboardValues();
+        }
+    }
+}
